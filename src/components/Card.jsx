064eecaa178be26ce65a4ba3bb77348b384692a1
w1nterm00n <kordesovad@gmail.com>
2024-04-25 16:42:13 +0400

    added ability to reverse two cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import '../styles/Card.css'
 
-function Card ({image, caption}) {
-    const [isReversed, setIsReversed] = useState(false);
+function Card ({id, image, caption, cardFlip}) {
+    const [isReversed, setIsReversed] = useState(true);
 
     const handleClick = () => {
         setIsReversed(!isReversed);
+        cardFlip(id, setIsReversed);   //передаёт, какая карта открылась
     };
     return (
         <div className={isReversed ? "card reversed" : "card"} onClick={handleClick}>
